591d6b64562799b744eaf9f3a9c84fc89b4dd598
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-11-19 22:22:42 +0330

    Implemented color generator

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,58 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState, useEffect } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("#f15025");
+  const [generateColor, setGenerateColor] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const generateTonsShades = function () {
+    try {
+      const rgbColor = new Values(color).all();
+      setGenerateColor(rgbColor);
+      setIsError(false);
+    } catch (error) {
+      setIsError(true);
+      console.error(`${error} 😒😒😒`);
+    }
+  };
+
+  useEffect(() => {
+    generateTonsShades();
+  }, []);
+
+  const formSubmitHandler = function (e) {
+    e.preventDefault();
+    generateTonsShades();
+    if (!e.target.value) setIsError(true);
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={formSubmitHandler}>
+          <input
+            className={`${isError && "error"}`}
+            type="text"
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          ></input>
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {generateColor &&
+          generateColor.map((el, index) => (
+            <SingleColor key={index} color={el} index={index} />
+          ))}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ color, index }) => {
+  const showColor = rgbToHex(...color.rgb); //color.rgb[0], color.rgb[1], color.rgb[2]
+  const [alert, setAlert] = useState(false);
+  const copyHandler = function () {
+    navigator.clipboard.writeText(showColor);
+    setAlert(true);
+  };
 
-export default SingleColor
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${color.rgb.join(",")})` }}
+      onClick={copyHandler}
+    >
+      <p className="percent-value">{color.weight}%</p>
+      <p className="color-value">{showColor}</p>
+      {alert && <p>Copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
