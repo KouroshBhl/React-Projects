05e4bce47af91535924319f3ac2d74ac1f08a27d
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-11-20 18:37:38 +0330

    Add functionallity to Navbar

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,37 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo" />
+        <button className="nav-toggle" onClick={() => setShowMenu(!showMenu)}>
+          <FaBars />
+        </button>
+      </div>
 
-export default Navbar
+      <div className={`links-container ${showMenu && "show-container"}`}>
+        <ul className="links">
+          {links.map((li) => (
+            <li key={li.id}>
+              <a href={li.url}>{li.text}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <ul className="social-icons">
+        {social.map((el) => (
+          <li key={el.id}>
+            <a href={el.href}>{el.icon}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
