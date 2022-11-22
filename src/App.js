86a2292cdc19a35c3e11da7a855e221ff1efb846
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-11-22 21:15:21 +0330

    Implement first basic routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>}></Route>
+        <Route path='about-us' element={<h1>About US</h1>}></Route>
+        <Route path='contact-us' element={<h2>Contact US!</h2>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
