0110dc4c082f92218b1c610ffbe06cb2ef9cd112
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-11-27 02:17:39 +0330

    Markdown finished

--- 16-markdown-preview/setup/src/App.js
+++ 16-markdown-preview/setup/src/App.js
@@ -2,7 +2,25 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [value, setValue] = useState('')
+  console.log(value)
+
+  return (
+    <main>
+      <section className='markdown'>
+        <textarea
+          name=''
+          className='input'
+          cols='30'
+          rows='10'
+          onChange={(e) => setValue(e.target.value)}
+        ></textarea>
+        <article className='result'>
+          <ReactMarkdown>{value}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  )
 }
 
 export default App
