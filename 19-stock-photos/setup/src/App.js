6d0ea1522e2dea3e9980cd492dc290bdb8a294ff
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-11-29 20:48:54 +0330

    Render photos from Unsplash API

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -4,9 +4,48 @@ import Photo from './Photo'
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_TOKEN}`
+//https://api.unsplash.com/photos/?client_id=FSls1ExG9-6bvvr6dAtcz80rZ6x3kG7f6kIhKhM7mvk
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+
+  const fetchPhotos = async function () {
+    try {
+      const response = await fetch(`${mainUrl}${clientID}`)
+      const data = await response.json()
+      console.log(data)
+      setPhotos(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
+
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form'>
+          <input type='text' placeholder='Search...' className='form-input' />
+          <button className='submit-btn' type='submit'>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((photo) => {
+            return <Photo key={photo.id} {...photo} />
+          })}
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default App
--- 19-stock-photos/setup/src/Photo.js
+++ 19-stock-photos/setup/src/Photo.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+const Photo = ({
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+  urls: { small },
+}) => {
+  return (
+    <article className='photo'>
+      <img src={small} alt={name} />
+      <div className='photo-info'>
+        <div>
+          <h4>{name}</h4>
+          <p>{likes} Likes</p>
+        </div>
+
+        <a href={portfolio_url}>
+          <img src={medium} alt={name} />
+        </a>
+      </div>
+    </article>
+  )
 }
 
 export default Photo
