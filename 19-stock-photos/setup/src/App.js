b1b475fe7d4cb29eb57afa81c740eca8382d8950
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-11-30 03:38:56 +0330

    Implemented infinity loading images

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,36 +1,82 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_TOKEN}`
-//https://api.unsplash.com/photos/?client_id=FSls1ExG9-6bvvr6dAtcz80rZ6x3kG7f6kIhKhM7mvk
 
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(0)
+  const [query, setQuery] = useState('')
 
   const fetchPhotos = async function () {
+    setLoading(true)
+    let url
+    const perPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
+    if (query) {
+      url = `${searchUrl}${clientID}${perPage}${urlQuery}`
+    } else {
+      url = `${mainUrl}${clientID}${perPage}`
+    }
     try {
-      const response = await fetch(`${mainUrl}${clientID}`)
+      const response = await fetch(url)
       const data = await response.json()
-      console.log(data)
-      setPhotos(data)
+
+      if (page === 1 && !query) {
+        setPhotos(data)
+      } else if (query && page === 1) {
+        setPhotos(data.results)
+      } else if (!query && page > 1) {
+        setPhotos((prevData) => [...prevData, ...data])
+      } else if (query && page > 1) {
+        setPhotos((prevData) => [...prevData, ...data.results])
+      } else {
+        setPhotos(data)
+      }
+      setLoading(false)
     } catch (error) {
+      setLoading(false)
       console.log(error)
     }
   }
-
   useEffect(() => {
     fetchPhotos()
+    //eslint-disable-next-line
+  }, [page])
+
+  const formSubmitHandler = function (e) {
+    e.preventDefault()
+    setPage(1)
+  }
+
+  useEffect(() => {
+    const scrollListener = window.addEventListener('scroll', function () {
+      if (
+        !loading &&
+        window.innerHeight + this.window.scrollY >=
+          this.document.body.scrollHeight - 50
+      ) {
+        setPage((prevPage) => prevPage + 1)
+      }
+    })
+
+    return () => window.removeEventListener('scroll', scrollListener)
+    //eslint-disable-next-line
   }, [])
 
   return (
     <main>
       <section className='search'>
-        <form className='search-form'>
-          <input type='text' placeholder='Search...' className='form-input' />
+        <form className='search-form' onSubmit={formSubmitHandler}>
+          <input
+            type='text'
+            placeholder='Search...'
+            className='form-input'
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button className='submit-btn' type='submit'>
             <FaSearch />
           </button>
@@ -39,9 +85,10 @@ function App() {
 
       <section className='photos'>
         <div className='photos-center'>
-          {photos.map((photo) => {
-            return <Photo key={photo.id} {...photo} />
-          })}
+          {!loading &&
+            photos.map((photo) => {
+              return <Photo key={photo.id} {...photo} />
+            })}
         </div>
       </section>
     </main>
