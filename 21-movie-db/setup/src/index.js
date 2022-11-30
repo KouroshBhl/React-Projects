3c069d2981e1a3d04614a35b49cffe5788a7aa42
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-11-30 16:22:37 +0330

    Remove some files

--- 21-movie-db/setup/src/index.js
+++ 21-movie-db/setup/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { AppProvider } from './context'
-import { BrowserRouter as Router } from 'react-router-dom'
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 )
