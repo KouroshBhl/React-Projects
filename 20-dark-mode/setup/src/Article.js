b6490e05172a733d9b202bf9d9ec2cd703207bd7
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-11-30 05:01:22 +0330

    use Intl for format date

--- 20-dark-mode/setup/src/Article.js
+++ 20-dark-mode/setup/src/Article.js
@@ -1,7 +1,24 @@
 import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
+const Article = ({ title, date, length, snippet }) => {
+  const options = {
+    weekday: 'long',
+    month: '2-digit',
+    year: 'numeric',
+    day: 'numeric',
+    timeZoneName: 'long',
+  }
+  const formatDate = new Intl.DateTimeFormat('en-US', options).format(date)
+
+  return (
+    <article className='post'>
+      <h2>{title}</h2>
+      <div className='post-info'>
+        <span>{formatDate}</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  )
 }
 
 export default Article
