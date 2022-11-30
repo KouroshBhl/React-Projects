08275e4d5a0611205ee6907c12fb0a51d990423b
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-11-30 05:23:28 +0330

    Implement dark theme and toggle button

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -3,12 +3,25 @@ import data from './data'
 import Article from './Article'
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem('style'))
+  useEffect(() => {
+    document.documentElement.className = theme
+    localStorage.setItem('style', theme)
+  }, [theme])
+
+  const toggleHandler = function () {
+    if (theme === 'dark') setTheme('light')
+    if (theme === 'light') setTheme('dark')
+  }
+
   return (
     <main>
       <nav>
         <div className='nav-center'>
           <h1>oveeeacted</h1>
-          <button className='btn'>toggle</button>
+          <button className='btn' onClick={toggleHandler}>
+            toggle
+          </button>
         </div>
       </nav>
       <section className='articles'>
