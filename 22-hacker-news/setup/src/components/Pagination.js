64d5730af4bfac432d7cca4a4c145a04e881c14a
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-01 17:56:02 +0330

    Fix pagination bug

--- 22-hacker-news/setup/src/components/Pagination.js
+++ 22-hacker-news/setup/src/components/Pagination.js
@@ -3,15 +3,16 @@ import { useGlobalContext } from '../helper/context'
 import * as config from '../helper/config'
 
 const Pagination = () => {
-  const { page, totalPages, results, getNews, search } = useGlobalContext()
+  const { page, totalPages, results, getNews, search, loading } =
+    useGlobalContext()
   const [pageNum, setPageNum] = useState(page)
   const nextPageHandler = function () {
     if (pageNum < totalPages) setPageNum((prevBtn) => prevBtn + 1)
-    if (pageNum === totalPages) setPageNum(0)
+    if (pageNum === totalPages - 1) setPageNum(0)
     getDataForPage()
   }
   const prevPageHandler = function () {
-    if (pageNum < 1) setPageNum(totalPages)
+    if (pageNum <= 1) setPageNum(totalPages - 1)
     if (pageNum >= 1) setPageNum((prevBtn) => prevBtn - 1)
     getDataForPage()
   }
@@ -22,11 +23,15 @@ const Pagination = () => {
   return (
     <div className='btn-container'>
       <span>{results} Results</span>
-      <button onClick={prevPageHandler}>prev</button>
+      <button disabled={loading} onClick={prevPageHandler}>
+        prev
+      </button>
       <p>
-        {pageNum} of {totalPages}
+        {pageNum + 1} of {totalPages}
       </p>
-      <button onClick={nextPageHandler}>next</button>
+      <button disabled={loading} onClick={nextPageHandler}>
+        next
+      </button>
     </div>
   )
 }
